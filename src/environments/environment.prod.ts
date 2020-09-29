9c95cff3b2d57b70343bab5d824a2ed3f2ca871f
Kit <deepakrahejain@gmail.com>
2020-09-29 20:28:28 +0530

    commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  isContentLoading: false,
+   
+
+  //*********************Production Server************* */
+  BASE_API_URL: 'http://34.67.65.213/EcommApiV3/api/',
+  ImagePath: 'http://34.67.65.213/EcommApiV3/ProductImage/',
+  APIURL: 'http://34.67.65.213/EcommApiV3',
+  Report_Path: 'http://localhost:44391/ReportHandler.ashx?'
+};
