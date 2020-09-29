563f569d953f84ecf57f03239546803e17a64434
Kit <deepakrahejain@gmail.com>
2020-09-29 20:38:01 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,5 @@ export const environment = {
   BASE_API_URL: 'http://34.67.65.213/EcommApiV3/api/',
   ImagePath: 'http://34.67.65.213/EcommApiV3/ProductImage/',
   APIURL: 'http://34.67.65.213/EcommApiV3',
-  Report_Path: 'http://localhost:44391/ReportHandler.ashx?'
+ Report_Path: 'http://34.67.65.213/EcommApiV3/ReportGenerate/',
 };
