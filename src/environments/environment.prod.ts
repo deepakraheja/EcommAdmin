489e8167baffdbf11e0fbc8c7acc1cccf137f3a6
Kit <deepakrahejain@gmail.com>
2020-11-18 14:58:37 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   isContentLoading: false,
-   
+
 
   //*********************Production Server************* */
-  BASE_API_URL: 'http://34.67.65.213/EcommApiV3/api/',
-  ImagePath: 'http://34.67.65.213/EcommApiV3/ProductImage/',
-  APIURL: 'http://34.67.65.213/EcommApiV3',
- Report_Path: 'http://34.67.65.213/EcommApiV3/ReportGenerate/',
+  BASE_API_URL: 'http://34.72.151.153/EcommApiV3/api/',
+  ImagePath: 'http://34.72.151.153/EcommApiV3/ProductImage/',
+  APIURL: 'http://34.72.151.153/EcommApiV3',
+  Report_Path: 'http://34.72.151.153/EcommApiV3/ReportGenerate/',
 };
