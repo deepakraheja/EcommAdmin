d8deb1ea5471294d3c3598ac15e0a0f42a9e0edc
Kit <deepakrahejain@gmail.com>
2021-01-07 16:02:49 +0530

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,17 @@ export const environment = {
 
 
   //*********************Production Server************* */
-  BASE_API_URL: 'http://34.72.151.153/EcommApiV3/api/',
-  ImagePath: 'http://34.72.151.153/EcommApiV3/ProductImage/',
-  APIURL: 'http://34.72.151.153/EcommApiV3',
-  Report_Path: 'http://34.72.151.153/EcommApiV3/ReportGenerate/',
+  // BASE_API_URL: 'http://34.72.151.153/EcommApiV3/api/',
+  // ImagePath: 'http://34.72.151.153/EcommApiV3/ProductImage/',
+  // APIURL: 'http://34.72.151.153/EcommApiV3',
+  // Report_Path: 'http://34.72.151.153/EcommApiV3/ReportGenerate/',
+
+
+
+  BASE_API_URL: 'http://103.108.220.24/EcommApiV3/api/',
+  ImagePath: 'http://103.108.220.24/EcommApiV3/ProductImage/',
+  APIURL: 'http://103.108.220.24/EcommApiV3',
+  Report_Path: 'http://103.108.220.24/EcommApiV3/ReportGenerate/',
+
+
 };
