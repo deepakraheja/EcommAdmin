4a8406ec287d90b85071c5e6927408b624763e0c
Kit <deepakrahejain@gmail.com>
2021-02-22 10:56:32 +0530

    commit

--- src/app/Reports/report-routing.module.ts
+++ src/app/Reports/report-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OrderReportComponent } from './order-report/order-report.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
-  { path: 'orderReport', component: OrderReportComponent },
+  { path: 'orderReport', component: OrderReportComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
